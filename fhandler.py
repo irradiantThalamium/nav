349619cc3e3cf3ac272e1d0c2388914ad8402ad1
tz <furshadow64@gmail.com>
2024-05-20 17:43:53 -0500

    .con change, moved log functions
    Changed conRead() to return a list for easier expandability later
    Moved the entire logging functionality into the file handler, because of common sense
    "//why didnt i do this shit e4rlier 464in?"

--- fhandler.py
+++ fhandler.py
@@ -1,22 +1,43 @@
 # handles .con and .log files
 
+from datetime import datetime
+
 def conRead(file):
     lfile = open(file, "r")
     cfgs = lfile.readlines()
     
-    fncfg = 0
+    fncfg=[0]
     
     for i in cfgs:
         x = x = i.split()
         if x[0] == "fullscreen" and int(x[1]) == 1:
-            fncfg += -2147483648 # this alone is making me regret doing this in python
+            fncfg[0] += -2147483648 # this alone is making me regret doing this in python
                                  # WHY ARE THERE NO UNSIGNED INTEGERS
                                  # fml
         elif x[0] == "autoscale" and int(x[1]) == 1:
-            fncfg += 512
+            fncfg[0] += 512
         elif x[0] == "bordered" and int(x[1]) == 0:
-            fncfg += 32
+            fncfg[0] += 32
 
     return fncfg
     
+now = datetime.now()
+
+def startLog():
+    logInit = now.strftime("%d-%m-%Y-%H%M%S")
+    logfl = open("logs/"+logInit+".txt", "w")
+    logfl.write(f"-- LOG START --\n- Time: {logInit} -\n")
+    logfl.close()
+    
+def log(txt):
+    logfl = open("logs/"+now.strftime("%d-%m-%Y-%H%M%S")+".txt", "a")
+    logfl.write(f"--{txt}\n")
+    logfl.close()
+    print(txt)
+    
+def endLog():
+    logFin = now.strftime("%d-%m-%Y-%H%M%S")
+    logfl = open("logs/"+logFin+".txt", "a")
+    logfl.write(f"-- LOG END --\n- Time: {logFin} -\n")
+    logfl.close()
     
--- main.py
+++ main.py
@@ -5,27 +5,16 @@ import pygame
 import random
 from datetime import datetime
 # v MY FILES v
-from fhandler import conRead
+from fhandler import conRead, startLog, log, endLog
 
 pygame.init()
-screen = pygame.display.set_mode((160, 120),conRead("startup.con"))
+screen = pygame.display.set_mode((160, 120),conRead("startup.con")[0])
 pygame.display.set_icon(pygame.image.load("icon.png"))
 pygame.display.set_caption("Null and Vøid | v0.0")
 clock = pygame.time.Clock()
 running = True
 
-now = datetime.now()
-logInit = now.strftime("%d-%m-%Y-%H%M%S")
-logfl = open("logs/"+logInit+".txt", "w")
-logfl.write(f"-- LOG START --\n- Time: {logInit} -\n")
-logfl.close()
-
-def log(txt):
-    logfl = open("logs/"+logInit+".txt", "a")
-    logfl.write(f"--{txt}")
-    logfl.close()
-    print(txt)
-
+startLog()
 while running:
     
     for event in pygame.event.get():
@@ -38,7 +27,5 @@ while running:
     
     clock.tick(60) 
 pygame.quit()
-logInit = now.strftime("%d-%m-%Y-%H%M%S")
-logfl = open("logs/"+logInit+".txt", "w")
-logfl.write(f"-- LOG END --\n- Time: {logInit} -\n")
-logfl.close()
+endLog()
+
