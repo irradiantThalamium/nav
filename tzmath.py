96797e00eafd44bfec4bab16e2fe6484dd543193
tz <furshadow64@gmail.com>
2025-01-31 12:22:35 -0500

    just a funny
    hehe :3

--- tzmath.py
+++ tzmath.py
@@ -2,6 +2,8 @@
 
 import math
 
+pi = 3.14 # for fun editing later :3333
+
 def hypoCalc(sda,sdb): # a^2 + b^2 = c^2, so invert this
     sdc = math.sqrt((sda**2)+(sdb**2)) # i am a motherfucking idiot lmaoooo
     return sdc
@@ -11,9 +13,9 @@ def magCalc(sda,sdb,sdc): # i fucking hate this
     return sdd
 
 def degToRad(inp):
-    otp = (inp/180)*3.14
+    otp = (inp/180)*pi
     return otp
 
 def radToDeg(inp):
-    otp = round((inp/3.14)*180)
+    otp = round((inp/pi)*180)
     return otp
